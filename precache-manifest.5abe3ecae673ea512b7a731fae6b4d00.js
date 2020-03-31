cf29fab66e31da4ed0855aa085a4b9a989fec375
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-31 11:05:12 +0000

    Deploy vvkk77/approver-dashboard to github.com/vvkk77/approver-dashboard.git:gh-pages

---
+++ precache-manifest.5abe3ecae673ea512b7a731fae6b4d00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a0d586881ae73cd643",
+    "url": "css/app.eaf6ce7e.css"
+  },
+  {
+    "revision": "ad115dd1a3a1b56dbcb9bb08c4a779d2",
+    "url": "img/empty-table.ad115dd1.png"
+  },
+  {
+    "revision": "423d5a42de0d7207f7d70a052f86593c",
+    "url": "img/login-bg.423d5a42.png"
+  },
+  {
+    "revision": "158687e21a53fb0ef1fc7031bed40a0d",
+    "url": "index.html"
+  },
+  {
+    "revision": "31a0d586881ae73cd643",
+    "url": "js/app.b0f83a2d.js"
+  },
+  {
+    "revision": "0e3638e48ec0e1c96eb2",
+    "url": "js/chunk-vendors.33bdc599.js"
+  },
+  {
+    "revision": "e879279f7c107d9faf44f0333b34acef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
